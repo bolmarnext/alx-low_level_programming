c6cff0a704fd1db9aba5866e66a3d26ffc723e3c
bolmarnext <bolmarc2020@gmail.com>
2023-08-12 05:33:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
