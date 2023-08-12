37bc92d55a264f41d4dcc5270f1172bcab9d343c
bolmarnext <bolmarc2020@gmail.com>
2023-08-12 01:16:54 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
- *main - main entry point
- *return : always 0 (success)
+ * main - A program that print a line with puts function
+ *
+ * Return: Always 0 (Success)
   */
 int main(void)
 {
