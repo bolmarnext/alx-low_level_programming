90326ff1865e718dad8621283430dd1c1b09afc3
bolmarnext <bolmarc2020@gmail.com>
2023-08-18 22:23:56 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * more_numbers - print more numbers
  */
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
